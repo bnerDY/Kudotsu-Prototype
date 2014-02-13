0854061b4772627d2ab6a48be2b96aa73c98c53f
Dingxin Yu <dingxin.yu@uqconnect.edu.au>
2014-02-14 07:22:53 +1000

    Add paypal feature

--- Demo-2013/Demo/Models/AccountViewModels.cs
+++ Demo-2013/Demo/Models/AccountViewModels.cs
@@ -81,5 +81,6 @@ namespace Demo.Models
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
+
     }
 }
